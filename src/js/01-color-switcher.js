5a68a942bb7e4f78d5fc4aeb039db9cf1dc2e8dc
Nick-Perelygin <77kalian77@gmail.com>
2023-03-27 19:24:36 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const startBtn = document.querySelector(`[data-start]`);
+const stoptBtn = document.querySelector(`[data-stop]`);
+const body = document.querySelector(`body`)
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {body.style.backgroundColor = getRandomHexColor()
+}, 1000);
+});
+
+stoptBtn.addEventListener(`click`, () => {
+     clearInterval(timerId);
+})
 
