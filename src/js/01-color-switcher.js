1b85d87dd5503a2691cb375f8d76cc5e42dc2172
Nick-Perelygin <77kalian77@gmail.com>
2023-04-03 23:32:55 +0300

    b

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
-const startBtn = document.querySelector(`[data-start]`);
-const stoptBtn = document.querySelector(`[data-stop]`);
-const body = document.querySelector(`body`)
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 let timerId = null;
 
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {body.style.backgroundColor = getRandomHexColor()
-}, 1000);
+startBtn.addEventListener ('click', () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
 });
 
-stoptBtn.addEventListener(`click`, () => {
-     clearInterval(timerId);
-})
-
+stopBtn.addEventListener ('click', () => {
+    clearInterval(timerId);
+});
