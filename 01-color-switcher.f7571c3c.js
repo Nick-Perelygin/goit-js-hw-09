c6e549d1bfae73494c3e1c5d7a2ecd344516ccf6
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-04-03 20:35:45 +0000

    Deploying to gh-pages from @ Nick-Perelygin/goit-js-hw-09@390810cc219914cff976315019aad143b9323688 🚀

--- 01-color-switcher.f7571c3c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.f7571c3c.js.map
